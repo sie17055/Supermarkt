a39a8bfb7d9fdeea53142518087543cb6ceb75c8
Alexander Siess <sie17055@spengergasse.at>
2016-12-16 11:16:36 +0100

    Klassen hinzugefügt

---
+++ Supermarkt/Filiale.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarkt
+{
+    class Filiale
+    {
+        int filialID;
+        string adresse;
+
+        public int FilialID
+        {
+            get
+            {
+                return filialID;
+            }
+
+            set
+            {
+                filialID = value;
+            }
+        }
+
+        public string Adresse
+        {
+            get
+            {
+                return adresse;
+            }
+
+            set
+            {
+                adresse = value;
+            }
+        }
+    }
+}
